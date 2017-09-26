c37848b6f59f7379908a9a52f241652adf3d0103
Dylan Reuter <djreuter45@gmail.com>
2017-09-25 19:57:47 -0600

    added jquery function to submit new task

---
+++ public_html/js/script.js
@@ -0,0 +1,17 @@
+// call the add task function
+add_task();
+
+function add_task() {
+	$('.add-new-task').submit(function() {
+		var new_task = $('.add-new-task input[name=new-task]').val();
+
+		if(new_task !== '') {
+			$.post('../php/add-task.php', { task: new_task}, function(data) {
+				$('.add-new-task input[name=new-task]').val('');
+				$(data).appendTo('.task-list ul').hide().fadeIn();
+			});
+		}
+		// make sure form doesn't submit twice
+		return false;
+	});
+}
