7050dc2eeba2e831ae3537a728ae02efd94a33c2
Dylan Reuter <djreuter45@gmail.com>
2017-09-25 19:38:29 -0600

    added javascript file

--- public_html/index.php
+++ public_html/index.php
@@ -20,6 +20,7 @@
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"
 				  integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1"
 				  crossorigin="anonymous"></script>
+		<script src="js/script.js" type="text/javascript"></script>
 		<title>PHP TODO List</title>
 	</head>
 	<body>
