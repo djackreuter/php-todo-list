2ec8d9205f751b0251e50d41f071e74a5935c80d
Dylan Reuter <djreuter45@gmail.com>
2017-10-02 17:03:25 -0600

    removed unused js file path

--- public_html/index.php
+++ public_html/index.php
@@ -52,7 +52,6 @@ $tasks = mysqli_query($mysqli, "SELECT * FROM tasks");
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"
 				  integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1"
 				  crossorigin="anonymous"></script>
-		<script src="js/script.js" type="text/javascript"></script>
 		<title>PHP TO-DO List</title>
 	</head>
 	<body>
